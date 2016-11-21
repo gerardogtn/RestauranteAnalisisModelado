b8b8084cb0d6e84d8c45743d8e51d93ba1371d27
Gerardo Teruel <gerardogtn@gmail.com>
2016-11-21 16:39:45 -0600

    Add LocalStock

---
+++ src/stock/LocalStock.hpp
@@ -0,0 +1,12 @@
+// Copyright 2016
+#ifndef STOCK_LOCALSTOCK_H
+#define STOCK_LOCALSTOCK_H
+
+class LocalStock {
+ public:
+  bool isEmpty() {
+    return true;
+  }
+};
+
+#endif  // STOCK_LOCALSTOCK_H
--- test/acceptance/StockEmployeeAcceptanceTest.cpp
+++ test/acceptance/StockEmployeeAcceptanceTest.cpp
@@ -1,6 +1,11 @@
 // Copyright 2016
 #include "gmock/gmock.h"
+#include "../../src/stock/LocalStock.hpp"
+
+using namespace testing;
 
 TEST(AStockEmployee, LocalStockIsEmptyWhenCreated) {
-  
+  LocalStock localStock;
+
+  ASSERT_THAT(localStock.isEmpty(), Eq(true));
 }
